b1bd944dd35d0fb0b9328febdfe9c0868560d6f0
Владимир <vova.perov.85@gmail.com>
2026-09-21 12:52:59 +0300

    Update views.py

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .form import RegistrationForm, TodoForm
-from django.contrib.auth import login, authenticate, logout
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, authenticate, logout
